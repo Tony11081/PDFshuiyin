3562920c842925a74bc0ecc7b4989a5d85b1b96d
Tony11081 <chengyadong1112@gmail.com>
2025-07-30 19:25:45 +0800

    Update: 更新项目代码

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,8 +2,6 @@
 
 import { useState, useEffect, useRef } from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
-import 'react-pdf/dist/esm/Page/TextLayer.css'
 
 // Set up PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
